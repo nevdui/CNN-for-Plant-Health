17cb2b928f535c71cb9274a12d7827f4b8ed151e
nevdui <109750713+nevdui@users.noreply.github.com>
2020-09-26 07:34:52 +0000

    Testing code

--- src/ghost_borrow_mut.rs
+++ src/ghost_borrow_mut.rs
@@ -1,2 +1,35 @@
 
 //! The `GhostBorrowMut` trait, which allows mutably borrowing multiple `GhostCell`s simultaneously.
+//!
+//! This module implement the `GhostBorrowMut` trait for:
+//!
+//! -   A slice of `GhostCell`s.
+//! -   An array of `GhostCell`s of any size.
+//! -   A tuple of `GhostCell`s of up to 12 elements.
+//! -   A tuple of references to `GhostCell`s of up to 12 elements.
+//!
+//! #   Performance
+//!
+//! In general borrowing is free of cost, however a special-case is necessary for the tuple of references, as then the
+//! references may alias.
+//!
+//! #   Experimental
+//!
+//! The feature is experimental, to enable, use the feature "experimental-multiple-mutable-borrows".
+
+use core::{convert::Infallible, mem, ptr};
+
+use crate::ghost_cell::*;
+
+/// An error signifying that two `GhostCell`s that need to be distinct were actually the same cell.
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug, PartialOrd, Ord)]
+pub struct GhostAliasingError;
+
+// For uniformity, if anyone wants it. Can't do
+// impl<T> From<Infallible> for T
+// because of conflicting implementations.
+impl From<Infallible> for GhostAliasingError {
+    fn from(_: Infallible) -> Self {
+        unreachable!("Infallible cannot be constructed")
+    }
+}
