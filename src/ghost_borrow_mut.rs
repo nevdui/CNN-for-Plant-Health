500232e97ab804b270a0a4a0596edc9b7b9267d7
nevdui <109750713+nevdui@users.noreply.github.com>
2020-09-27 10:14:46 +0000

    add a new post

--- src/ghost_borrow_mut.rs
+++ src/ghost_borrow_mut.rs
@@ -33,3 +33,35 @@ impl From<Infallible> for GhostAliasingError {
         unreachable!("Infallible cannot be constructed")
     }
 }
+
+/// A trait for implementing multiple borrows for any number of arguments, using a `GhostToken<'a, 'brand>`.
+///
+/// Implemented for a mixture of tuple and array types.
+///
+/// #   Experimental
+///
+/// The feature is experimental, to enable, use the feature "experimental-multiple-mutable-borrows".
+pub trait GhostBorrowMut<'a, 'brand> {
+    /// The references you get as a result.
+    ///
+    /// For example, if `Self` is `(&'a GhostCell<'brand, T0>, &'a GhostCell<'brand, T1>)` then `Result` is
+    /// `(&'a mut T0, &'a mut T1)`.
+    type Result;
+
+    /// The error case.
+    ///
+    /// Use a never type such as `!` or `Infallible` if an error is impossible, and `GhostAliasingError` otherwise.
+    type Error: Into<GhostAliasingError>;
+
+    /// Borrows any number of `GhostCell`s mutably at the same time.
+    ///
+    /// If any of them are the same `GhostCell`, returns `Error`.
+    ///
+    /// #   Performance
+    ///
+    /// In general, borrowing should be free of cost if possible. This can be ascertained by checking the error type:
+    /// if it is a never type, then the operation is infallible, indicating no run-time check is necessary.
+    ///
+    /// If the operation is not infallible, then a runtime check is necessary, in which case the unchecked version of
+    /// the operation may be used if performance matters and the caller is certain of the absence of problems.
+    ///
