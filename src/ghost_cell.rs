6abe34be8d8dbffb6f75a403d47483809131a031
nevdui <109750713+nevdui@users.noreply.github.com>
2020-10-31 20:18:43 +0000

    Add a setup.py

--- src/ghost_cell.rs
+++ src/ghost_cell.rs
@@ -1,3 +1,3 @@
 //! `GhostCell` and `GhostToken`, as per <https://plv.mpi-sws.org/rustbelt/ghostcell/>.
 //!
-//! Reference implementation at <https://gi
+//! Reference implementation at <https://gitlab.mpi-sws.org/FP/ghostcell/-/tree/mast
