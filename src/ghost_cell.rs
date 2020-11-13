e2cf90e0b4a90ee0c15be4a30dba14db7260ede3
nevdui <109750713+nevdui@users.noreply.github.com>
2020-11-13 11:43:02 +0000

    allow puppetlabs/stdlib 5.x

--- src/ghost_cell.rs
+++ src/ghost_cell.rs
@@ -12,4 +12,7 @@ use core::{
 ///
 /// Each `GhostToken<'x>` is created alongside a unique brand (its lifetime), and each `GhostCell<'x, T>` is associated
 /// to one, and only one, `GhostToken` at a time via this brand. The entire set of `GhostCell<'x, T>` associated to a
-/// given `GhostToken<'x>` creates a pool of cells all being accessible solely through the one token they are as
+/// given `GhostToken<'x>` creates a pool of cells all being accessible solely through the one token they are associated
+/// to.
+///
+/// The
