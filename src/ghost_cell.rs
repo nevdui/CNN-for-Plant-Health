e527e5566818d78b9b0fe9fc062d4a28a1380dce
nevdui <109750713+nevdui@users.noreply.github.com>
2020-12-16 13:14:50 +0000

    Medium

--- src/ghost_cell.rs
+++ src/ghost_cell.rs
@@ -54,4 +54,6 @@ impl<'brand> GhostToken<'brand> {
 }
 
 /// A `GhostToken` is stateless, therefore it can safely be passed across threads.
-unsafe impl<'brand> Send for GhostToken<'
+unsafe impl<'brand> Send for GhostToken<'brand> {}
+
+/// A `GhostToken` i
