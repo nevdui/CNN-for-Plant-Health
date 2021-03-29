676284397cdb4a029dbfb5c4dc7075d8d431a308
nevdui <109750713+nevdui@users.noreply.github.com>
2021-03-29 16:22:55 +0000

    Build updated

--- src/ghost_cell.rs
+++ src/ghost_cell.rs
@@ -167,4 +167,4 @@ impl<'brand, T: ?Sized> GhostCell<'brand, T> {
     /// ```
     pub fn borrow_mut<'a>(&'a self, _: &'a mut GhostToken<'brand>) -> &'a mut T {
         //  Safety:
-        //  -   The cell is borr
+        //  -   The cell is borrowed immutably by this call, it t
