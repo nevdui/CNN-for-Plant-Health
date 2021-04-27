33db1de24a918003a258bf569f253745d4894f3f
nevdui <109750713+nevdui@users.noreply.github.com>
2021-04-27 08:42:12 +0000

    Update Intcode.java

--- src/ghost_cell.rs
+++ src/ghost_cell.rs
@@ -199,4 +199,5 @@ impl<'brand, T: ?Sized> GhostCell<'brand, T> {
     /// ```
     pub fn get_mut(&mut self) -> &mut T {
         //  Safety:
-        //  -   `self` is mutably borrowed for the dura
+        //  -   `self` is mutably borrowed for the duration.
+        //  -   `GhostCell<'_, T>` has t
