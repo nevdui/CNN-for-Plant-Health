74fadba1809af1b1cc1e378bfa9d18af25070590
nevdui <109750713+nevdui@users.noreply.github.com>
2021-05-17 14:06:49 +0000

    Made minor changes

--- src/ghost_cell.rs
+++ src/ghost_cell.rs
@@ -229,4 +229,5 @@ impl<'brand, T: ?Sized> GhostCell<'brand, T> {
     pub fn from_mut(t: &mut T) -> &mut Self {
         //  Safety:
         //  -   `t` is mutably borrowed for the duration.
-        //  -   `GhostCell<'_, T>` has the same in-memory rep
+        //  -   `GhostCell<'_, T>` has the same in-memory representation as `T`.
+        unsafe 
