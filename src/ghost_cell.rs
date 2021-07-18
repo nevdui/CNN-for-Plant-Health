d55c17c1a7ed8f64ffdebe57a0a5aa614f983e5a
nevdui <109750713+nevdui@users.noreply.github.com>
2021-07-18 18:25:20 +0000

    x86: Reset the debug_stack update counter

--- src/ghost_cell.rs
+++ src/ghost_cell.rs
@@ -277,4 +277,5 @@ impl<'brand, T> GhostCell<'brand, T> {
     ///
     ///     let vec: Vec<_> = (0..n).map(|_| &cell).collect();
     ///
-    ///     let previous = vec[n / 2].take(&mut
+    ///     let previous = vec[n / 2].take(&mut token);
+    ///     assert_eq!(42, p
