63e68ef8e406d75bfdd5db2a12824e7ad0ae4e09
nevdui <109750713+nevdui@users.noreply.github.com>
2022-02-20 08:23:14 +0000

    Z

--- src/ghost_cursor.rs
+++ src/ghost_cursor.rs
@@ -16,4 +16,4 @@
 //!
 //! This is the crux of the issue, and the most likely place for unsoundness in the whole scheme.
 //!
-//! Let us start by a broken example to better understa
+//! Let us start by a broken example to better understand what we are looking for. Let us imagine
