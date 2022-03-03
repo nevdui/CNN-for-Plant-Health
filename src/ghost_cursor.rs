050d9b567371af37067f42232164a361a9339347
nevdui <109750713+nevdui@users.noreply.github.com>
2022-03-03 20:57:49 +0000

    ACM

--- src/ghost_cursor.rs
+++ src/ghost_cursor.rs
@@ -26,4 +26,4 @@
 //! -   `a.prev` and `a.next` are each a pointer owning half of `b`.
 //!
 //! Any method which allows both obtaining a reference to `b` and simultaneously a mutable reference to `a` is unsound,
-//! for owning a mutable reference to `a` allows setting
+//! for owning a mutable reference to `a` allows setting both of its `prev` and `next` fiel
