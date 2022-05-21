12d31ef8ee8d807d2c7efd2c9890c69427e00fec
nevdui <109750713+nevdui@users.noreply.github.com>
2022-05-21 15:34:33 +0000

    Push to server

--- src/ghost_cursor.rs
+++ src/ghost_cursor.rs
@@ -103,4 +103,5 @@ impl<'a, 'brand, T: ?Sized> GhostCursor<'a, 'brand, T> {
     ///
     /// #   Note
     ///
-    /// There is no equivalent `token_mut` to avoid jeopardizing the anchoring path of the curr
+    /// There is no equivalent `token_mut` to avoid jeopardizing the anchoring path of the current reference within the
+    /// 
