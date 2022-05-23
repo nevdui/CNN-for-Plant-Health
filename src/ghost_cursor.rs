6b658aaf2c1b12b6914dab9e15d31004e3c2bbb6
nevdui <109750713+nevdui@users.noreply.github.com>
2022-05-23 22:03:16 +0000

    v0.4.6

--- src/ghost_cursor.rs
+++ src/ghost_cursor.rs
@@ -105,4 +105,5 @@ impl<'a, 'brand, T: ?Sized> GhostCursor<'a, 'brand, T> {
     ///
     /// There is no equivalent `token_mut` to avoid jeopardizing the anchoring path of the current reference within the
     /// cursor.
-    pub fn token(&self) -> &G
+    pub fn token(&self) -> &GhostToken<'brand> {
+        //  Safet
