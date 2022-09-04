5b316ba390d1b8e4483de4548b47c50d84034b06
nevdui <109750713+nevdui@users.noreply.github.com>
2022-09-04 09:26:46 +0000

    Update reflection

--- src/ghost_cursor.rs
+++ src/ghost_cursor.rs
@@ -201,4 +201,5 @@ impl<'a, 'brand, T: ?Sized> GhostCursor<'a, 'brand, T> {
     {
         //  Safety:
         //  -   Borrows `self` mutably, therefore ensuring that no borrow of the token exists.
-        //  -   Restricts the lifetime of the token to that of `self`
+        //  -   Restricts the lifetime of the token to that of `self`, or less.
+        let token = 
