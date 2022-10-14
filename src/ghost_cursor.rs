e11db61cfc3255124df1ef48fb563394ba30867d
nevdui <109750713+nevdui@users.noreply.github.com>
2022-10-14 21:13:52 +0000

    added .gitignore.

--- src/ghost_cursor.rs
+++ src/ghost_cursor.rs
@@ -239,4 +239,7 @@ impl<'a, 'brand, T: ?Sized> GhostCursor<'a, 'brand, T> {
     ///
     ///     if let Some(s) = cursor.borrow_mut() {
     ///         let previous = std::mem::replace(s, "Station".to_string());
-    ///         assert_eq!("Terminal", p
+    ///         assert_eq!("Terminal", previous);
+    ///     }
+    /// });
+  
