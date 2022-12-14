4a1a8e6d0e82b230a90760e23eb7fd816256c28c
nevdui <109750713+nevdui@users.noreply.github.com>
2022-12-14 16:53:48 +0000

    Updates mm.md

--- src/ghost_cursor.rs
+++ src/ghost_cursor.rs
@@ -316,4 +316,6 @@ pub fn cursor_into_inner_leaves_token_borrowed_mutably() {}
 /// use ghost_cell::{GhostCell, GhostCursor, GhostToken};
 ///
 /// GhostToken::new(|mut token| {
-///     let (one, two) = (GhostCell::new(1), GhostCell::new
+///     let (one, two) = (GhostCell::new(1), GhostCell::new(2));
+///
+///     let curs
